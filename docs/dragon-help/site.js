ed10f666679d79df8c52ac798a676397a26e53c1
Saplonily <Saplonily@outlook.com>
2022-10-11 11:42:25 +0800

    迁移东西,鼠标特效更改页面

--- docs/dragon-help/site.js
+++
@@ -1,4 +0,0 @@
-const targetElement = document.querySelector("#ghost")
-targetElement.style.width = 100;
-targetElement.style.height = 100;
-new cursoreffects.trailingCursor({particles: 15});
---
+++ index.js
@@ -0,0 +1,4 @@
+const targetElement = document.querySelector("html")
+targetElement.style.width = 10;
+targetElement.style.height = 10;
+new cursoreffects.trailingCursor({particles: 5});
