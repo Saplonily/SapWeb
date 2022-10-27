14774366b0453521f4dd95d50e772bc179ef0c2f
Saplonily <Saplonily@outlook.com>
2022-10-27 12:11:46 +0800

    加一个图片...?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const targetElement = document.querySelector("html")
 targetElement.style.width = 10;
 targetElement.style.height = 10;
-new cursoreffects.trailingCursor({particles: 5});
+new cursoreffects.trailingCursor({ particles: 5 });
